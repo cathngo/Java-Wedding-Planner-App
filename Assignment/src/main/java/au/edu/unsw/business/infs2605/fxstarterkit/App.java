e15fe72f865cf2b5fb0b07518810e13b4c2b0885
mimimmiii <61225296+mimimmiii@users.noreply.github.com>
2020-04-02 18:11:02 +1100

    Database

--- Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
+++ Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
@@ -5,6 +5,8 @@
  */
 package au.edu.unsw.business.infs2605.fxstarterkit;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author jaydenso
@@ -14,8 +16,15 @@ public class App {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+       //DatabaseManager.createEvent();
+       //DatabaseManager.eventData();
+      // DatabaseManager.guestData();
+      //DatabaseManager.createRsvp();
+       //DatabaseManager.rsvpData();
+       DatabaseManager.setupDatabaseOnFirstRun();
+       DatabaseManager.printInvitation();
     }
     
 }
