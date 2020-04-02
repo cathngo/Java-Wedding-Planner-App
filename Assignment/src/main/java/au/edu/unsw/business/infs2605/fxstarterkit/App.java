d7929bde450941f75006eb78e7ef6e025c2cb17b
mimimmiii <61225296+mimimmiii@users.noreply.github.com>
2020-04-02 18:51:01 +1100

    fixed up Assignment folder

--- Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
+++ Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD:src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package au.edu.unsw.business.infs2605.fxstarterkit;
-
-import java.sql.SQLException;
-
-/**
- *
- * @author jaydenso
- */
-public class App {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SQLException {
-        // TODO code application logic here
-    //Method to set up database if needed to
-       DatabaseManager.setupDatabaseOnFirstRun();
-       //Methods to print out all entries in a table
-       DatabaseManager.printEvent();
-       DatabaseManager.printGuest();
-       DatabaseManager.printAdmin();
-       DatabaseManager.printInvitation();
-       DatabaseManager.printRsvp();
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -61,4 +28,3 @@ public class App {
     }
     
 }
->>>>>>> e15fe72f865cf2b5fb0b07518810e13b4c2b0885:Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/App.java
