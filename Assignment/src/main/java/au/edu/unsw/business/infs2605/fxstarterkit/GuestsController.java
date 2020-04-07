b7be9ccdf44f25ec7d99b44c6a55288862e2a110
Catherine <cngo1080@gmail.com>
2020-04-07 15:16:01 +1000

    tableview for guest added

--- Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/GuestsController.java
+++ Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/GuestsController.java
@@ -7,7 +7,12 @@ package au.edu.unsw.business.infs2605.fxstarterkit;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
@@ -17,6 +22,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 /**
  * FXML Controller class
  *
@@ -25,12 +31,7 @@ import javafx.scene.control.TableView;
 public class GuestsController implements Initializable {
 
 
-    @FXML
-    private TableView<?> tableGuests;
-    @FXML
-    private TableColumn<?, ?> nameColumn;
-    @FXML
-    private TableColumn<?, ?> emailColumn;
+   
     @FXML
     private Button btnInviteGuest;
     @FXML
@@ -39,19 +40,64 @@ public class GuestsController implements Initializable {
     private Button btnViewGuests;
     @FXML
     private AnchorPane guestsPane;
+      @FXML
+    private TableView<TableGuest> guest_table;
+   
+    @FXML
+    private TableColumn<TableGuest,String> col_firstName;
+   @FXML
+    private TableColumn<TableGuest,String> col_lastName;
+    @FXML
+    private TableColumn<TableGuest,String> col_guestEmail;
+   
+    ObservableList<TableGuest>guestList = FXCollections.observableArrayList();
+    
+    
+
+  
+     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        try{
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:mydatabase.db");
+            ResultSet rs = conn.createStatement().executeQuery("select * from guest");
+            
+            while (rs.next()){
+                guestList.add(new TableGuest( rs.getString("guest_fname"),
+                 rs.getString("guest_lname"),
+              rs.getString("guest_email")));
+            }
+            
+        }catch(Exception e){
+            System.out.println("table not created");
+        }
+        
+        
+        col_firstName.setCellValueFactory(new PropertyValueFactory<>("guest_fname"));
+        
+ 
+        col_lastName.setCellValueFactory(new PropertyValueFactory<>("guest_lname"));
+        col_guestEmail.setCellValueFactory(new PropertyValueFactory<>("guest_email"));
+       
+        
+        guest_table.setItems(guestList);
+    }
+        
+   
     /**
      * Initializes the controller class.
      */   
     
     @FXML
     private void loadInviteGuest(ActionEvent event) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("invitateguest.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("inviteguest.fxml"));
         guestsPane.getChildren().setAll(pane);
     }
 
     @FXML
     private void loadCreateGuest(ActionEvent event) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("createguest.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("createGuest.fxml"));
         guestsPane.getChildren().setAll(pane);
     }
 
@@ -61,9 +107,6 @@ public class GuestsController implements Initializable {
         guestsPane.getChildren().setAll(pane);
     }
     
-        @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    } 
+       
 
 }
---
+++ Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/TableGuest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.edu.unsw.business.infs2605.fxstarterkit;
+
+/**
+ *
+ * @author cathy
+ */
+public class TableGuest {
+    String guest_fname;
+    String guest_lname;
+    String guest_email;
+
+    public TableGuest(String guest_fname, String guest_lname, String guest_email) {
+        this.guest_fname = guest_fname;
+        this.guest_lname = guest_lname;
+        this.guest_email = guest_email;
+    }
+
+    public String getGuest_fname() {
+        return guest_fname;
+    }
+
+    public void setGuest_fname(String guest_fname) {
+        this.guest_fname = guest_fname;
+    }
+
+    public String getGuest_lname() {
+        return guest_lname;
+    }
+
+    public void setGuest_lname(String guest_lname) {
+        this.guest_lname = guest_lname;
+    }
+
+    public String getGuest_email() {
+        return guest_email;
+    }
+
+    public void setGuest_email(String guest_email) {
+        this.guest_email = guest_email;
+    }
+
+    
+            
+}
