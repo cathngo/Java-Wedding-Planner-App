9bae7b79b912426f7ad6809034d4f7f3058ef5fd
Catherine <cngo1080@gmail.com>
2020-04-12 19:55:44 +1000

    merge branch

--- Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/A_ViewEventInviteGuestController.java
+++ Assignment/src/main/java/au/edu/unsw/business/infs2605/fxstarterkit/A_ViewEventInviteGuestController.java
@@ -12,17 +12,25 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 /**
  *
@@ -39,16 +47,30 @@ import javafx.scene.text.Text;
     private TableColumn<Guest,String> col_lname;
     @FXML
     private Text eventName;
+    
+    @FXML
+    private TableView<Guest> guestListTable;
+    @FXML
+    private TableColumn<Guest,String> col_guestListFname;
+    @FXML
+    private TableColumn<Guest,String> col_guestListLname;
+     @FXML
+    private TableColumn<Guest, CheckBox> col_select;
    
+    
     @FXML
     private AnchorPane eventPane;
     
+   
+    
+    
+   
     
 
      
 
 
-   
+    
     ObservableList<Guest>guestList = FXCollections.observableArrayList();
     
     
@@ -63,12 +85,16 @@ import javafx.scene.text.Text;
             ResultSet rs = conn.createStatement().executeQuery("select * from guest");
             
             while (rs.next()){
+                
+           
                 guestList.add(new Guest( rs.getString("guest_fname"),
-             
               rs.getString("guest_lname")));
            
+                
+                
+                
             }
-            
+           
             conn.close();
             rs.close();
         }catch(Exception e){
@@ -78,12 +104,69 @@ import javafx.scene.text.Text;
         
         col_fname.setCellValueFactory(new PropertyValueFactory<>("guest_fname"));
         col_lname.setCellValueFactory(new PropertyValueFactory<>("guest_lname"));
+      
+        
+       
+
+     
+        col_select.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Guest, CheckBox>, ObservableValue<CheckBox>>() {
+
+      
+            @Override
+            public ObservableValue<CheckBox> call(
+                    TableColumn.CellDataFeatures<Guest, CheckBox> select) {
+                Guest user = select.getValue();
+
+                CheckBox checkBox = new CheckBox();
+
+                checkBox.selectedProperty().setValue(checkBox.isSelected());
+
+
+
+                checkBox.selectedProperty().addListener(new ChangeListener<Boolean>() {
+                    public void changed(ObservableValue<? extends Boolean> ov,
+                            Boolean old_val, Boolean new_val) {
+
+                       checkBox.setSelected(new_val);
+
+                    }
+                });
+
+                return new SimpleObjectProperty<CheckBox>(checkBox);
+
+            }
+
+        });
+  
+        
+        
+        
+        
+        
+        
         existingGuestTable.setItems(guestList);
+        existingGuestTable.getColumns().addAll(col_select);
         
         
     }
     
-    
+    @FXML
+    private void btnAddToGuestListWasClicked(ActionEvent event){
+        for (Guest g : existingGuestTable.getItems()){
+            if (g.getCheckBox().isSelected()){
+                Platform.runLater(() -> {
+                
+            existingGuestTable.getItems().remove(g);
+            
+                    
+                    
+                    
+                    
+            });
+            }
+            
+        }
+    }
     
     @FXML
     private void btnInviteNewGuestWasClicked(ActionEvent event) throws IOException {
